2d860fc5cf74496f7dfee64dcfc9249b16ec9b12
Abiodun Aremu <abiodun.milliscript@gmail.com>
2022-10-20 21:40:30 +0100

    Update db cred
    Update db credential

--- database/mysqlDB.js
+++ database/mysqlDB.js
@@ -1,9 +1,9 @@
 "user strict";
 const connection = {
-  host: "******",
-  user: "report",
-  password: "report123",
-  database: "oicdb",
+  host: "ltnya0pnki2ck9w8.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+  user: "ajld07dggdap7e5e",
+  password: "ttojmnu3xtxb8i62",
+  database: "q5uxwbwigssrvep1",
 };
 
 module.exports = connection;
